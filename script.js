57b22ff1d772200a07cc0b6f65585533c1bcae28
MarutiBandagar9121 <marut.bandagar9121@gmail.com>
2024-08-07 11:10:02 +0530

    slick slider  added to the skills section

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const typingText = Typify("#auto-text", {
-  text: ["Computer Enthusiast !","Coder","Developer !"],
+  text: ["Web Developer!","Digital Dreamer","Code Creator"],
   delay: 100,
   loop: true,
   cursor: true,
-  stringDelay: 1000,
+  stringDelay: 700,
 });
 
 
@@ -27,4 +27,20 @@ $(document).ready(function () {
   $(".dropdown-menu-content a").click(function () {
     $(".dropdown-menu-content").toggle();
   });
+
+  
+
+$('.skills-parent-container').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
+  autoplay:true,
+  autoplaySpeed: 1000,
+  draggable:true,
+  prevArrow:'<i class="fa-solid fa-circle-chevron-left fa-lg prev-button" style="color: #000000;"></i>',
+  nextArrow:'<i class="fa-solid fa-circle-chevron-right fa-lg next-button" style="color: #000000;"></i>'
+});
 });
